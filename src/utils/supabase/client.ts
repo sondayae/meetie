f70700036abe2d659df20b740665dfcf5e5b611a
sondayae <sondayae@gmail.com>
2024-08-18 17:35:29 +0900

    [주석/손다예] supabase 공통 로직 수정

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,10 +1,12 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabase = () => {
+const createClient = () => {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
-}
+  )
+};
+
+const supabase = createClient();
 
 export default supabase;
