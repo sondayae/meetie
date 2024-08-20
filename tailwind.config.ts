014caaae56812a49504060c35c669dfca7b4251a
sondayae <sondayae@gmail.com>
2024-08-20 17:16:41 +0900

    [주석/손다예] tailwind config 색상 export 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'main-purple': '#6224FD',
+        'sub-purple': '#8655FF',
+        'light-purple': '#F5F1FF',
+        'gray-purple': '#82829B',
+        'dark-gray': '#444444',
+        'disabled': '#D0D6E0',
+        'middle-gray': '#DDDDDD',
+        'light-gray': '#F5F5F5',
+      }
     },
   },
   plugins: [],
