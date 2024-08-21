7743f3a9217373fba6ca86bef06e345f762bfcb2
Jiyeon Han <chocorogit@gmail.com>
2024-08-21 10:51:56 +0900

    [공통/한지연] 공통 layout.tsx 수정, 공통 pretended 폰트 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/stories/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
         'main-purple': '#6224FD',
@@ -20,10 +20,19 @@ const config: Config = {
         'light-purple': '#F5F1FF',
         'gray-purple': '#82829B',
         'dark-gray': '#444444',
-        'disabled': '#D0D6E0',
+        disabled: '#D0D6E0',
         'middle-gray': '#DDDDDD',
         'light-gray': '#F5F5F5',
-      }
+      },
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      fontWeight: {
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+      },
     },
   },
   plugins: [],
