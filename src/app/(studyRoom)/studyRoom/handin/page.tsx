bac7bc9409b19e1db7e5fbc94b7ed382225881b6
sondayae <sondayae@gmail.com>
2024-08-21 19:08:10 +0900

    [스터디룸/TRMS-4/손다예] 과제 인증 조회 페이지 추가

--- src/app/(studyRoom)/studyRoom/handin/page.tsx
+++ src/app/(studyRoom)/studyRoom/handin/page.tsx
@@ -1,18 +1,26 @@
 'use client';
 import { useEffect, useState } from 'react';
 import supabase from '@/utils/supabase/client';
-// import { getHandIn } from "./api";
+import MoreIcon from '@/components/icons/MoreIcon';
 
 const page = () => {
-  const [notes, setNotes] = useState<any[]>([])
+  const [handinList, setHandinList] = useState<any[]>([])
   const fetchData = async () => {
     const {data} = await supabase.from('handin').select(`
       id,
       text,
-      images,
-      user ( name )`);
+      created_at,
+      images ( url ),
+      user ( name )`)
+      .order('created_at', { ascending: false });
+
     console.log(data);
-    setNotes(data || []);
+    setHandinList(data || []);
+  }
+
+  const dateFormatter = (timestamp: string) => {
+    const date = new Date(timestamp).toLocaleString().slice(0,-3);
+    return date;
   }
 
   useEffect(() => {
@@ -20,15 +28,49 @@ const page = () => {
   }, []);
 
   return (
-    <div>
-      {notes.map((data) => (
-        <div key={data.id}>
-          <p>{data.user.name}</p>
-          <p>{data.text}</p>
+    <div className="bg-[#FAFAFA]">
+      <div className="border-b-2 border-middle-gray p-[34px]">
+        <p>과제 일정</p>
+        <p>주차별 과제 현황을 확인하고 소통해요.</p>
+        <div className="rounded-lg bg-light-purple p-3">
+          <p>우리는 이렇게 소통해요</p>
         </div>
-      ))}
+      </div>
+      <div className="p-[13px]">
+        <p>6월</p>
+        캘린더 영역
+      </div>
+      <div className="rounded-md bg-white">
+        <div className="border-b-2 border-middle-gray pb-[24px] pt-[37px]">
+          <p>6월 4일 화요일</p>
+          <p>과제를 인증한 팀원들을 확인해 보세요.</p>
+        </div>
+        <div>
+          {handinList.map((handin) => (
+            <div
+              key={handin.id}
+              className="grid grid-rows-2 grid-cols-2 border-b-2 border-b-middle-gray p-[30px]"
+            >
+              <div className='col-span-2'>
+                <span>유저 이미지</span>
+                <span className="pr-[30px]">{handin.user.name}</span>
+                <span>14일차 과제</span>
+                <MoreIcon className="fill-black w-5 h-5"/>
+              </div>
+              <div className='row-span-2 col-span-2'>
+                <p>{handin.text}</p>
+                <img
+                  src={handin.images ? handin.images.url : ''}
+                  className="w-20"
+                />
+                <p>{dateFormatter(handin.created_at)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default page;
