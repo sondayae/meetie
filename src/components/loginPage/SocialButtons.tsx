b2ca45fe1297830039c647da38b130bdc5b5c7f0
황준용 <wnsdyd3333@gmail.com>
2024-08-23 10:56:24 +0900

    [로그인/TRMS-3/황준용] 소셜 로그인 로직 추가

--- src/components/loginPage/SocialButtons.tsx
+++ src/components/loginPage/SocialButtons.tsx
@@ -1,13 +1,37 @@
+'use client';
+
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { Provider } from '@supabase/supabase-js';
+
 import GithubIcon from '@/components/icons/GithubIcon';
 import GoogleIcon from '@/components/icons/GoogleIcon';
 import KakaoIcon from '@/components/icons/KakaoIcon';
+import supabase from '@/utils/supabase/client';
 
 function SocialButtons() {
+  const signInWithSocial = async (provider: Provider) => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    });
+    console.log(data, error);
+  };
   return (
     <div className="flex justify-center gap-6">
-      <GithubIcon width={46} height={46} />
-      <KakaoIcon width={46} height={46} />
-      <GoogleIcon width={46} height={46} />
+      <button type="button" onClick={() => signInWithSocial('github')}>
+        <GithubIcon width={46} height={46} />
+      </button>
+      <button type="button" onClick={() => signInWithSocial('kakao')}>
+        <KakaoIcon width={46} height={46} />
+      </button>
+      <button type="button" onClick={() => signInWithSocial('google')}>
+        <GoogleIcon width={46} height={46} />
+      </button>
     </div>
   );
 }
