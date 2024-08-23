cf05ab3e757d34ee59b360e1f143fe050caaf9e9
황준용 <wnsdyd3333@gmail.com>
2024-08-23 10:39:08 +0900

    [로그인/TRMS-3/황준용] ID 비번 찾기 컴포넌트 생성

---
+++ src/components/loginPage/AccountRecoveryLinks.tsx
@@ -0,0 +1,12 @@
+function AccountRecoveryLinks() {
+  return (
+    <div className="my-16 flex justify-center gap-3">
+      <div className="text-xs font-medium text-gray-purple">회원가입하기</div>
+      <div className="border-x-[1px] border-[#c4c4c4] px-3 text-xs font-medium text-[#ADB5Bd]">
+        아이디 찾기
+      </div>
+      <div className="text-xs font-medium text-[#ADB5Bd]">비밀번호 찾기</div>
+    </div>
+  );
+}
+export default AccountRecoveryLinks;
