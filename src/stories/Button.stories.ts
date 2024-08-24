e54b2df48d5068a1e9bbbbb743c2ffd07081763a
sondayae <sondayae@gmail.com>
2024-08-24 14:08:04 +0900

    Buttons.stories.ts Meta 추가

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -5,12 +5,18 @@ import Button from '../components/common/Button';
 const meta = {
 	title: 'Component/Button',
 	component: Button,
+  args: {
+    primary: true,
+    label: 'Button',
+    size: 'small' || 'medium' || 'large',
+    borderStyle: 'none',
+  },
   tags: ['autodocs'],
-};
+} satisfies Meta<typeof Button>;
 
 export default meta;
 
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
