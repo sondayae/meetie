070d7f57c8bfd66cad68bb3fd0554662cabd2462
황준용 <wnsdyd3333@gmail.com>
2024-08-28 04:24:20 +0900

    소셜 아이콘 constants 추가

---
+++ src/constants/socialIcons.ts
@@ -0,0 +1,11 @@
+import GithubIcon from '@/components/icons/GithubIcon';
+import GoogleIcon from '@/components/icons/GoogleIcon';
+import KakaoIcon from '@/components/icons/KakaoIcon';
+
+const SOCIAL_ICONS = [
+  { name: 'github', icon: GithubIcon, bgColor: 'bg-[#3c4043]' },
+  { name: 'kakao', icon: KakaoIcon, bgColor: 'bg-[#fae500]' },
+  { name: 'google', icon: GoogleIcon, bgColor: 'bg-[#f8f8f8]' },
+];
+
+export default SOCIAL_ICONS;
