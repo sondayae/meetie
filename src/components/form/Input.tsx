df58646a16ab69e0ddca3e70a10c30bab96cfe7d
황준용 <wnsdyd3333@gmail.com>
2024-08-28 05:05:24 +0900

    인풋 및 에러메세지 컴포넌트 스타일 수정

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -40,7 +40,7 @@ export default function Input<T extends FieldValues>({
         </label>
       )}
       <div
-        className={`flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
+        className={`mt-5 flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
       >
         <input
           className={`w-full flex-initial border-none px-4 py-[14px] placeholder:text-gray-purple focus:outline-none ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'}`}
