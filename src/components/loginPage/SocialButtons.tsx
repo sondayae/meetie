faf477f393fc8a0922bb9ba0fd0e3ba35a45e1ba
황준용 <wnsdyd3333@gmail.com>
2024-08-28 04:27:02 +0900

    소셜 로그인 버튼 로직 수정

--- src/components/loginPage/SocialButtons.tsx
+++ src/components/loginPage/SocialButtons.tsx
@@ -1,37 +1,34 @@
 'use client';
 
-/* eslint-disable jsx-a11y/control-has-associated-label */
 import { Provider } from '@supabase/supabase-js';
 
-import GithubIcon from '@/components/icons/GithubIcon';
-import GoogleIcon from '@/components/icons/GoogleIcon';
-import KakaoIcon from '@/components/icons/KakaoIcon';
+import SOCIAL_ICONS from '@/constants/socialIcons';
 import supabase from '@/utils/supabase/client';
 
 function SocialButtons() {
   const signInWithSocial = async (provider: Provider) => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    await supabase.auth.signInWithOAuth({
       provider,
-      options: {
-        queryParams: {
-          access_type: 'offline',
-          prompt: 'consent',
-        },
-      },
+      options: { redirectTo: `http://localhost:3000/api/auth/callback` },
     });
-    console.log(data, error);
   };
+
+  const socialButtonClass =
+    'flex h-11 w-11 items-center justify-center rounded shadow';
+
   return (
     <div className="flex justify-center gap-6">
-      <button type="button" onClick={() => signInWithSocial('github')}>
-        <GithubIcon width={46} height={46} />
-      </button>
-      <button type="button" onClick={() => signInWithSocial('kakao')}>
-        <KakaoIcon width={46} height={46} />
-      </button>
-      <button type="button" onClick={() => signInWithSocial('google')}>
-        <GoogleIcon width={46} height={46} />
-      </button>
+      {SOCIAL_ICONS.map((social) => (
+        <button
+          className={`${socialButtonClass} ${social.bgColor}`}
+          key={social.name}
+          type="button"
+          aria-label={social.name}
+          onClick={() => signInWithSocial(social.name as Provider)}
+        >
+          <social.icon />
+        </button>
+      ))}
     </div>
   );
 }
