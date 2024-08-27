574994207a65f344787f7004519c6055ad71e0d4
황준용 <wnsdyd3333@gmail.com>
2024-08-28 04:25:14 +0900

    Style 수정

--- src/components/loginPage/AccountRecoveryLinks.tsx
+++ src/components/loginPage/AccountRecoveryLinks.tsx
@@ -1,6 +1,6 @@
 function AccountRecoveryLinks() {
   return (
-    <div className="my-16 flex justify-center gap-3">
+    <div className="flex justify-center gap-3 py-16">
       <div className="text-xs font-medium text-gray-purple">회원가입하기</div>
       <div className="border-x-[1px] border-[#c4c4c4] px-3 text-xs font-medium text-[#ADB5Bd]">
         아이디 찾기
