97c8c15ffde3e8ad77cb2f3a3458b0bc52e45717
황준용 <wnsdyd3333@gmail.com>
2024-08-28 05:10:53 +0900

    인풋 컴포넌트 스타일 수정

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -33,14 +33,14 @@ export default function Input<T extends FieldValues>({
   const hasError = !!errors[name];
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="mt-5 flex flex-col gap-2">
       {label && (
         <label className="font-semibold leading-6 text-[#000417]" htmlFor={id}>
           {label}
         </label>
       )}
       <div
-        className={`mt-5 flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
+        className={`flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
       >
         <input
           className={`w-full flex-initial border-none px-4 py-[14px] placeholder:text-gray-purple focus:outline-none ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'}`}
