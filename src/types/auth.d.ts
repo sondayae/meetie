f13eb728a3e18c378fc8845cd429855274ec973a
황준용 <wnsdyd3333@gmail.com>
2024-08-29 04:32:33 +0900

    회원가입 폼 타입 정의

--- src/types/auth.d.ts
+++ src/types/auth.d.ts
@@ -2,3 +2,10 @@ export interface LoginFormData {
   email: string;
   password: string;
 }
+
+export interface SignUpFormData {
+  email: string;
+  password: string;
+  passwordCheck: string;
+  name: string;
+}
