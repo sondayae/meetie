87370841b8591b16775582490e17ee09ff7ccb66
황준용 <wnsdyd3333@gmail.com>
2024-08-29 10:04:23 +0900

    비밀번호 찾기 서버 액션 구현

---
+++ src/components/findPasswordPage/actions/findPassword.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { headers } from 'next/headers';
+
+import { FindPasswordFormData } from '@/types/auth';
+import { createClient } from '@/utils/supabase/server';
+
+const findPassword = async (formData: FindPasswordFormData) => {
+  const origin = headers().get('origin');
+  const supabase = createClient();
+
+  const { error: matchError } = await supabase
+    .from('user')
+    .select('email')
+    .eq('email', formData.email)
+    .single();
+
+  if (matchError) {
+    return {
+      success: false,
+      type: 'notFound',
+      message: '존재하지 않는 정보 입니다.',
+    };
+  }
+
+  const { error: resetError } = await supabase.auth.resetPasswordForEmail(
+    formData.email,
+    {
+      redirectTo: `${origin}/login/update/password`,
+    },
+  );
+
+  if (resetError) {
+    throw new Error(resetError.message);
+  }
+
+  return { success: true };
+};
+
+export default findPassword;
