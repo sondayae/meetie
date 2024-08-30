6bebd3cfb1d4cbc3c2a9239c74be6376f6da6fc7
tidyp <tidyp1030@icloud.com>
2024-08-30 17:00:25 +0900

    edit studyDetail

--- src/components/study/StatusDisplay.tsx
+++ src/components/study/StatusDisplay.tsx
@@ -6,27 +6,55 @@ import supabase from '@/utils/supabase/client';
 import { useEffect, useState } from 'react';
 import { getacceptedApplyUser } from '@/hooks/useStudy';
 
-export default function Footer() {
+export default function StatusDisplay() {
   const accessNum = getacceptedApplyUser();
-  console.log(accessNum);
-  const isLeader = true;
-  // const isLeader = false;
+
   const params = useParams();
   const path = usePathname();
   const displayRequest = path.endsWith('studyrequest');
+
   console.log(params.studyId);
 
+  const isLeader = true;
+  // const isLeader = false;
+
+  // TODO: api 분리
   const postApply = async () => {
+    const userId = '10669baa-7476-4f40-8bbf-37ba8765de74';
+
     try {
-      const { data, error } = await supabase.from('study_apply').insert([
+      // 신청 상태 확인
+      const { data, error } = await supabase
+        .from('study_apply')
+        .select('*')
+        .eq('studyId', params.studyId)
+        .eq('userId', userId);
+
+      if (error) {
+        throw error;
+      }
+
+      // 신청 정보가 존재하는 경우
+      if (data && data.length > 0) {
+        alert('이미 신청한 상태입니다.');
+        return;
+      }
+
+      // 신청 정보가 없는 경우
+      const { data2, error2 } = await supabase.from('study_apply').insert([
         {
-          // 요청 페이지 id
           studyId: params.studyId,
-          // userId: '705bccf5-d3f9-4aac-9bbb-582a5edcab87',
-          userId: '10669baa-7476-4f40-8bbf-37ba8765de74',
+          userId: userId,
           status: 'wating',
         },
       ]);
+
+      if (error2) {
+        throw error2;
+      }
+
+      console.log(data2);
+      alert('신청이 완료되었습니다.');
     } catch (error) {
       alert('예상치 못한 문제가 발생하였습니다. 다시 시도하여 주십시오.');
     }
@@ -43,8 +71,8 @@ export default function Footer() {
 
   return (
     <>
-      <div className="fixed bottom-0 mx-auto h-[#104px] w-full max-w-[600px] items-center justify-center px-4">
-        <div className="flex w-full items-center justify-center gap-5 bg-white py-4 text-[14px]">
+      <div className="fixed bottom-0 h-[#104px] w-full max-w-[600px] items-center justify-center px-8">
+        <div className="flex items-center justify-center gap-5 bg-white py-4 text-[14px]">
           <div className="flex w-full flex-col items-center">
             <p className="text-[14px]">참여가능인원</p>
             <p className="text-bold text-[18px]">
@@ -71,19 +99,12 @@ export default function Footer() {
             {/* TODO: 스터디원 => 요청(상태)*/}
             {!isLeader && (
               <>
-                <button
-                  type="button"
-                  // primary
-                  // label="참가 요청"
+                <Button
+                  type="primary"
+                  size="large"
+                  label="참가 요청"
                   onClick={() => postApply()}
-                >
-                  참가요청
-                </button>
-                {/* <Button
-                     primary
-                     label="참가 요청"
-                     onClick={() => postApply()}
-                   ></Button> */}
+                ></Button>
               </>
             )}
           </div>
