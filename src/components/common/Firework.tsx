4c03a93a383c49e522557fd0a196104bac37bc94
sondayae <sondayae@gmail.com>
2024-09-02 14:16:11 +0900

    폭죽 컴포넌트 및 css 추가

--- src/components/common/Firework.tsx
+++ src/components/common/Firework.tsx
@@ -1,7 +1,7 @@
 export default function FireWork() {
   return (
     <>
-    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite] top-[50%] left-[50%]'>
+    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite] -top-[30%] -right-[20%] scale-[0.8]'>
       <div class="explosion"></div>
       <div class="explosion"></div>
       <div class="explosion"></div>
@@ -11,7 +11,7 @@ export default function FireWork() {
       <div class="explosion"></div>
       <div class="explosion"></div>
     </div>
-    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite_600ms] top-[45%] left-[40%] scale-[0.6]'>
+    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite_600ms] top-[30%] -left-[40%] scale-[0.6]'>
       <div class="explosion"></div>
       <div class="explosion"></div>
       <div class="explosion"></div>
@@ -21,7 +21,7 @@ export default function FireWork() {
       <div class="explosion"></div>
       <div class="explosion"></div>
     </div>
-    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite_400ms] top-[65%] left-[60%] scale-[0.4]'>
+    <div className='absolute [&_div]:before:animate-[explosion_2s_infinite_200ms] bottom-[40%] right-0 scale-[0.4]'>
       <div class="explosion"></div>
       <div class="explosion"></div>
       <div class="explosion"></div>
