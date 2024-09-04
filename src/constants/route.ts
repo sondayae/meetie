8193767f484ade2f0447b9aa39bf6982207ce4e6
황준용 <wnsdyd3333@gmail.com>
2024-09-04 09:13:44 +0900

    라우트 경로 추가

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -5,6 +5,12 @@ const ROUTE_PATH = {
     FIND_PASSWORD: '/login/find/password',
     UPDATE_PASSWORD: '/login/update/password',
   },
+  WALKTHROUGH: '/walkthrough',
+  MEMBER: {
+    PROFILE: {
+      CREATE: '/profile/create',
+    },
+  },
 };
 
 export default ROUTE_PATH;
