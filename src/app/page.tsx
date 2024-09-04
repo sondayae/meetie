b59d19be2cbe29d911499623e0730e4a1c41c99d
황준용 <wnsdyd3333@gmail.com>
2024-09-04 09:50:59 +0900

    홈 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import Link from 'next/link';
 
-import LaptopIcon from '@/components/icons/LaptopIcon';
-import MessageIcon from '@/components/icons/MessageIcon';
+import HomeSVG from '@/components/icons/HomeSVG';
 import MoreIcon from '@/components/icons/MoreIcon';
-import NutIcon from '@/components/icons/NutIcon';
-import RocketIcon from '@/components/icons/RocketIcon';
-import SquareIcon from '@/components/icons/SquareIcon';
 import AccountRecoveryLinks from '@/components/loginPage/AccountRecoveryLinks';
 import SocialButtons from '@/components/loginPage/SocialButtons';
 import ROUTE_PATH from '@/constants/route';
@@ -22,32 +18,8 @@ export default async function Home() {
         </span>
       </div>
 
-      <div className="relative m-auto mb-3 mt-20 h-[332px] w-full overflow-hidden">
-        <SquareIcon
-          width={150}
-          height={150}
-          className="absolute bottom-0 right-0 -translate-y-[20%] translate-x-[25%]"
-        />
-        <NutIcon
-          width={120}
-          height={120}
-          className="absolute bottom-0 left-0 -translate-x-1/2 -translate-y-1/2"
-        />
-        <LaptopIcon
-          width={167}
-          height={126}
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
-        />
-        <MessageIcon
-          width={68}
-          height={38}
-          className="animate-messageMove absolute left-[58%] top-14 z-10"
-        />
-        <RocketIcon
-          className="animate-rocketMove absolute left-1/2 top-1/2"
-          width={104}
-          height={159}
-        />
+      <div className="mb-3 mt-20 flex h-[332px] w-full items-center justify-center overflow-hidden">
+        <HomeSVG className="h-full w-auto pr-11" />
       </div>
 
       <div className="flex justify-center gap-6">
