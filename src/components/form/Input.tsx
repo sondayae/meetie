1715efe5f2e280a8fec93ef0618376cc9b552e4a
황준용 <wnsdyd3333@gmail.com>
2024-09-05 12:34:18 +0900

    인풋 컴포넌트 확장

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -16,6 +16,7 @@ interface InputProps<T extends FieldValues>
   register: UseFormRegister<T>;
   rules: RegisterOptions<T, Path<T>>;
   errors: FieldErrors<T>;
+  as?: 'input' | 'textarea';
 }
 
 export default function Input<T extends FieldValues>({
@@ -29,8 +30,10 @@ export default function Input<T extends FieldValues>({
   register,
   rules,
   errors,
+  as = 'input',
 }: InputProps<T>) {
   const hasError = !!errors[name];
+  const Comp = as === 'input' ? 'input' : 'textarea';
 
   return (
     <div className="mt-5 flex flex-col gap-2">
@@ -42,12 +45,13 @@ export default function Input<T extends FieldValues>({
       <div
         className={`flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
       >
-        <input
+        <Comp
           className={`w-full flex-initial border-none px-4 py-[14px] placeholder:text-gray-purple focus:outline-none ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'}`}
           id={id}
-          type={type}
+          type={as === 'input' ? type : undefined}
           placeholder={placeholder}
           {...register(name, rules)}
+          rows={as === 'textarea' ? 4 : undefined}
         />
         {children}
       </div>
