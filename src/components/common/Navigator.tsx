6e5e6b7b8b14577cfde79eeb74ca781b1e62dca1
sondayae <sondayae@gmail.com>
2024-09-06 14:14:13 +0900

    공통 스터디 아바타 및 바텀 시트 추가

--- src/components/common/Navigator.tsx
+++ src/components/common/Navigator.tsx
@@ -1,47 +1,59 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { twMerge } from 'tailwind-merge';
+
 import Chat from '../icons/Navigator/Chat';
 import Lamp from '../icons/Navigator/Lamp';
 import My from '../icons/Navigator/My';
 import Search from '../icons/Navigator/Search';
-import { usePathname } from 'next/navigation';
-import { twMerge } from 'tailwind-merge';
 
 export default function Navigator() {
   const path = usePathname();
-  
-  
-  
-  const menus = [{
-    icon: <Lamp className='stroke-current'/>,
-    name: '스터디룸',
-    path: '/studyroom'
-  }, {
-    icon: <Search className='stroke-current'/>,
-    name: '탐색',
-    path: '/search'
-  }, {
-    icon: <Chat className='stroke-current'/>,
-    name: '채팅',
-    path: '/chat'
-  }, {
-    icon: <My className='stroke-current'/>,
-    name: '마이페이지',
-    path: '/mypage'
-  }];
+
+  const menus = [
+    {
+      icon: <Lamp className="stroke-current" />,
+      name: '스터디룸',
+      path: '/studyroom',
+    },
+    {
+      icon: <Search className="stroke-current" />,
+      name: '탐색',
+      path: '/search',
+    },
+    {
+      icon: <Chat className="stroke-current" />,
+      name: '채팅',
+      path: '/chat',
+    },
+    {
+      icon: <My className="stroke-current" />,
+      name: '마이페이지',
+      path: '/mypage',
+    },
+  ];
   return (
-    <div className='flex bg-white justify-around py-2 drop-shadow'>
-      {menus.map(menu => {
+    <div className="flex justify-around bg-white py-2 drop-shadow">
+      {menus.map((menu) => {
         return (
-        <Link key={menu.name} href={menu.path}>
-          <div className={twMerge('flex flex-col gap-1 items-center justify-center text-xs', path.includes(menu.path) ? 'text-main-purple' : 'text-gray-purple')}>
-            <span>{menu.icon}</span>
-            <span>{menu.name}</span>
-          </div>
-        </Link>
-        )
+          <Link key={menu.name} href={menu.path}>
+            <div
+              className={twMerge(
+                'flex flex-col items-center justify-center gap-1 text-xs',
+                path.includes(menu.path)
+                  ? 'text-main-purple'
+                  : 'text-gray-purple',
+              )}
+            >
+              <span>{menu.icon}</span>
+              <span>{menu.name}</span>
+            </div>
+          </Link>
+        );
       })}
     </div>
-  )
+  );
 }
