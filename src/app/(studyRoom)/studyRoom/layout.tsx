013a07ed0e1a5ecd7750d838dc2ef4a5e478ddfd
sondayae <sondayae@gmail.com>
2024-09-07 11:16:14 +0900

    스터디룸 메인 페이지 디자인 레이아웃 수정 및 애니메이션 추가

--- src/app/(studyRoom)/studyRoom/layout.tsx
+++ src/app/(studyRoom)/studyRoom/layout.tsx
@@ -3,10 +3,8 @@ import Header from '@/components/handin/Header'
 
 export default function layout({children}: {children: React.ReactNode}) {
   return (
-    <>
-    <div className='flex flex-col h-full'>
-        <div className='flex-grow'>{children}</div>
+    <div className='grid grid-rows-[1fr_60px] h-full'>
+        {children}
     </div>
-    </>
   )
 }
