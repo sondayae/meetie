5b537ab4bfa0fb8e5d9ed308fd257da02e944e6f
황준용 <wnsdyd3333@gmail.com>
2024-09-09 16:23:54 +0900

    선택된 날짜 일정 get api 라우트

---
+++ src/apis/calendar.ts
@@ -0,0 +1,16 @@
+import { format } from 'date-fns';
+
+export const getSchedule = async (date: Date) => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/calendar?date=${format(date, 'yyyy-MM-dd')}`,
+  );
+
+  if (!response.ok) {
+    const { error } = await response.json();
+    throw new Error(error.message);
+  } else {
+    const data = await response.json();
+
+    return data;
+  }
+};
