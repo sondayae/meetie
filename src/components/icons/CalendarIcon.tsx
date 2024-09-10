5d2631bcd2d63eb616fada43fdd8f822058f8ca4
sondayae <sondayae@gmail.com>
2024-09-11 00:45:46 +0900

    스터디룸 캘린더 페이지 헤더 및 내비게이터 추가

--- src/components/icons/CalendarIcon.tsx
+++ src/components/icons/CalendarIcon.tsx
@@ -1,4 +1,6 @@
-export default function CalendarIcon() {
+import { Icon } from '@/types/icon';
+
+export default function CalendarIcon({className}: Icon) {
   return (
     <svg
       width="40"
@@ -6,6 +8,7 @@ export default function CalendarIcon() {
       viewBox="0 0 40 40"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={className}
     >
       <path
         d="M27 12H13C11.8954 12 11 12.8954 11 14V28C11 29.1046 11.8954 30 13 30H27C28.1046 30 29 29.1046 29 28V14C29 12.8954 28.1046 12 27 12Z"
