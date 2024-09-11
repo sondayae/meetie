5784f28ee8137438ddef5c6a16a49a195415b30d
tidyp <tidyp1030@icloud.com>
2024-09-11 13:55:11 +0900

    design study Detail layout 수정

---
+++ src/actions/studyapply.action.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import supabase from '@/utils/supabase/client';
+
+export const postApply = async (params: string, userId: string) => {
+  console.log(params, userId)
+  try {
+    const { data, error } = await supabase
+      .from('study_apply')
+      .select('*')
+      .eq('studyId', params)
+      .eq('userId', userId);
+
+    if (error) {
+      throw error;
+    }
+
+    const { data: data2, error: error2 } = await supabase
+      .from('study_apply')
+      .insert([
+        {
+          studyId: params,
+          userId: userId,
+          status: 'waiting',
+        },
+      ]);
+
+    if (error2) {
+      throw error2;
+    }
+
+    // return 
+  } catch (error) {
+    alert('예상치 못한 문제가 발생하였습니다. 다시 시도하여 주십시오.');
+  }
+};
+
+export const deleteApply = async (params: string, userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from('study_apply')
+      .delete()
+      .eq('studyId', params)
+      .eq('userId', userId);
+
+    if (error) {
+      throw error;
+    }
+
+  } catch (error) {
+    alert('예상치 못한 문제가 발생하였습니다. 다시 시도하여 주십시오.');
+  }
+};
--- src/components/study/StudyMain.tsx
+++ src/components/study/StudyMain.tsx
@@ -1,30 +1,19 @@
-// 'use client'
+'use client';
 import { format } from 'date-fns';
 import Link from 'next/link';
 import ProfileAvatar from '@/components/common/ProfileAvatar';
 import Image from 'next/image';
 import EyeIcon from '../icons/EyeIcon';
+import Button from '@/components/common/Button';
 
-interface Study {
-  title: string;
-  endDate: string;
-  startDate: string;
-  created_at: string;
-  viewCount: number;
-  goal: string;
-  info: string;
-  recruitNum: number;
-  tags: string[];
-  user: {
-    id: string;
-    name: string;
-    images?: {
-      url: string;
-    };
-  };
-}
+import { StudyDetail } from '@/types/studydetail';
+import { useState } from 'react';
+import { postApply, deleteApply } from '@/actions/studyapply.action';
 
 export default function StudyMain({
+  userdata,
+  isApply,
+  params,
   title,
   endDate,
   startDate,
@@ -32,21 +21,40 @@ export default function StudyMain({
   viewCount,
   goal,
   info,
-  recruitNum,
   tags,
   user,
-}: Study) {
+  recruitNum,
+  isRecruiting,
+}: StudyDetail) {
+  const [btnisApply, setbtnisApply] = useState(isApply);
+
   const ddays = Math.round(
     (Number(new Date(endDate)) - Number(new Date())) / 1000 / 60 / 60 / 24,
   );
 
+  const isAuthor = userdata?.id === user?.id;
+  console.log(isAuthor)
+
+  const handleApply = async (type: boolean) => {
+    if (type) {
+      postApply(params.studyId, userdata.id);
+      setbtnisApply((prev) => !prev);
+    } else {
+      deleteApply(params.studyId, userdata.id);
+      setbtnisApply((prev) => !prev);
+    }
+  };
+
   return (
     <>
-      <section className="flex flex-col justify-center px-4 pb-[14px] pt-6 border-b-2 border-[#F1F2F6]">
+      <section className="flex flex-col justify-center border-b-2 border-[#F1F2F6] px-4 pb-[14px] pt-6">
         {/* 1 */}
         <div className="mb-5 flex items-center gap-[14px]">
           {/* title */}
-          <p className="text-[24px] font-semibold">{title}</p>
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[24px] font-semibold">
+            {title}
+          </p>
+
           {/* d-day */}
           <div className="flex h-6 w-14 items-center justify-center gap-2 rounded-full border border-[#8346ff] bg-white p-1.5">
             <div className="text-xs font-medium text-[#8346ff]">
@@ -103,29 +111,101 @@ export default function StudyMain({
       </section>
 
       <main className="flex flex-col gap-8 p-4">
-        <main className="flex flex-col gap-8">
-          <div className="flex flex-col gap-4 text-[#434343]">
-            <p className="font-semibold">스터디 주제</p>
-            <p className="whitespace-pre text-[15px]">{info}</p>
+        {/* <main className="flex flex-col gap-8"> */}
+        <div className="flex flex-col gap-4 text-[#434343]">
+          <p className="font-semibold">스터디 주제</p>
+          <div className="whitespace-pre-line text-[15px]">
+            {info.split('\n').map((line, idx) => (
+              <div key={idx}>
+                {line.includes('https') ? (
+                  <Link className="underline" href={line} target="_black">
+                    {line}
+                  </Link>
+                ) : (
+                  line
+                )}
+              </div>
+            ))}
           </div>
-          <div className="flex flex-col gap-4 text-[#434343]">
-            <p className="whitespace-break-spaces font-semibold">스터디 목표</p>
-            <p className="text-[15px]">{goal}</p>
-          </div>
-          <div className="flex flex-col gap-4 text-[#434343]">
-            <p className="font-semibold">스터디 인원</p>
-            <p className="text-[15px]">{recruitNum} 명</p>
-          </div>
-          <div className="flex flex-col gap-4 text-[#434343]">
-            <p className="font-semibold">스터디 기간</p>
-            <p className="text-[15px]">
-              <span>{format(startDate, 'yyyy-MM-dd')}</span>
-              <span>&nbsp;~ &nbsp;</span>
-              <span>{format(endDate, 'yyyy-MM-dd')}</span>
-            </p>
-          </div>
-        </main>
+        </div>
+        <div className="flex flex-col gap-4 text-[#434343]">
+          <p className="whitespace-break-spaces font-semibold">스터디 목표</p>
+          <p className="text-[15px]">{goal}</p>
+        </div>
+        <div className="flex flex-col gap-4 text-[#434343]">
+          <p className="font-semibold">스터디 인원</p>
+          <p className="text-[15px]">{recruitNum} 명</p>
+        </div>
+        <div className="flex flex-col gap-4 text-[#434343]">
+          <p className="font-semibold">스터디 기간</p>
+          <p className="text-[15px]">
+            <span>{format(startDate, 'yyyy-MM-dd')}</span>
+            <span>&nbsp;~ &nbsp;</span>
+            <span>{format(endDate, 'yyyy-MM-dd')}</span>
+          </p>
+        </div>
       </main>
+      {/* </main> */}
+
+      <div className="sticky bottom-0 flex min-h-[104px] max-w-[600px] justify-between border-t-[1px] border-[#DDDDDD] bg-white p-2 px-4 py-7">
+        {/* 신청마감 */}
+        {!isRecruiting && (
+          <Button type="disabled" label="모집이 마감되었습니다."></Button>
+        )}
+        {/* 신청중 => 로그인x */}
+        {isRecruiting && !userdata?.id && (
+          <Link className="w-full" href={`/login`}>
+            <Button type="disabled" label="로그인 후 신청하기" />
+          </Link>
+        )}
+        {/* 신청여부확인 => 로그인O */}
+        {isRecruiting && userdata?.id && (
+          <div className="flex w-full gap-[19px]">
+            <div className="flex w-[74px] flex-col items-center">
+              <div className="w-[74px] text-sm font-medium text-[#81819b]">
+                참여가능인원
+              </div>
+              <div>
+                <span className="text-lg font-medium leading-normal text-[#6224fd]">
+                  2명
+                </span>
+                <span className="text-lg font-medium leading-normal text-[#9d9d9d]">
+                  {' '}
+                  /{' '}
+                </span>
+
+                <span className="text-lg font-medium leading-normal text-[#6f6f6f]">
+                  {recruitNum}명
+                </span>
+              </div>
+            </div>
+            {/* 신청여부확인 => 로그인O => 작성자*/}
+            {isAuthor && (
+              <Link className="w-full" href={`/study/${params.studyId}/studyrequest`}>
+                <Button
+                  type="primary"
+                  label="대기 중인 요청 확인"
+                  onClick={() => {}}
+                />
+              </Link>
+            )}
+            {!isAuthor && (
+              <Button
+                type={btnisApply ? 'disabled' : 'primary'}
+                size="large"
+                label={!btnisApply ? '신청하기' : '신청취소'}
+                onClick={async () => {
+                  if (!btnisApply) {
+                    await handleApply(true);
+                  } else {
+                    await handleApply(false);
+                  }
+                }}
+              />
+            )}
+          </div>
+        )}
+      </div>
     </>
   );
 }
