c2e2ecfabf81deac398d0e9e2384512d567c67c0
황준용 <wnsdyd3333@gmail.com>
2024-09-12 08:35:41 +0900

    인풋 컴포넌트 수정

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -43,10 +43,10 @@ export default function Input<T extends FieldValues>({
         </label>
       )}
       <div
-        className={`flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'border-[#e9e9e9] focus-within:border-main-purple hover:border-sub-purple'} tab`}
+        className={`flex items-center overflow-hidden rounded-lg border ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'} ${hasError ? 'border-red-400' : 'focus-within:border-main-purple hover:border-sub-purple border-[#e9e9e9]'} tab`}
       >
         <Comp
-          className={`w-full flex-initial border-none px-4 py-[14px] placeholder:text-muted-foreground focus:outline-none ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'}`}
+          className={`w-full flex-initial border-none px-4 py-[14px] placeholder:text-muted-foreground focus:outline-none ${Comp === 'textarea' ? 'resize-none' : ''} ${background === 'white' ? 'bg-white' : 'bg-[#F9F9F9]'}`}
           id={id}
           type={as === 'input' ? type : undefined}
           placeholder={placeholder}
