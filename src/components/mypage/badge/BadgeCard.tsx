16d024cb97f9920185badef2628254f7eaed49de
sondayae <sondayae@gmail.com>
2024-09-19 22:46:57 +0900

    뱃지 상세 로직 추가

--- src/components/mypage/badge/BadgeCard.tsx
+++ src/components/mypage/badge/BadgeCard.tsx
@@ -1,37 +1,60 @@
-'use client';
-
-type Badge = {
-  title: string;
-  name: string;
-  src: string;
-}
-
 import {
-  Card
-} from "@/components/ui/card"
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { dateFormatter } from '@/utils/common/dateFormatter';
+import { getBadgeImgUrl } from '@/utils/supabase/storage';
+import { twMerge } from 'tailwind-merge';
 
-
-export default function BadgeCard() {
-  // { badge, type }: {badge: Badge, type: string}
-  // const title = badge.name;
+export default function BadgeCard({badge, isAwarded, awardedAt}: {badge: any, isAwarded: boolean|undefined, awardedAt: string}) {
   
-  function getDisplayName(title: string) {
-    if (title.includes('beginner')) {
-        return '뉴비';
-    } else if (title.includes('runner')) {
-        return '러너';
-    } else if (title.includes('master')) {
-        return '마스터';
-    } else {
-        return title;
+  const getType = (type: string) => {
+    let typeText = '';
+    switch(type) {
+      case 'comment': typeText = '댓글뱃지'; break;
+      case 'feedback': typeText = '피드뱃지'; break;
+      case 'study': typeText = '스터디뱃지'; break;
+      case 'meett': typeText = '밋티뱃지'; break;
     }
+    return typeText;
   }
+
   return (
-    <Card>
-      <div className='flex flex-col'>
-        <span>뱃지 이미지</span>
-        <span>뱃지 이름</span>
+    <Drawer>
+    {isAwarded ? (
+    <DrawerTrigger>
+      <div className='flex flex-col items-center gap-2 cursor-pointer'>
+        <img src={getBadgeImgUrl(badge.image_path)} alt={badge.name} className={twMerge('min-w-[150px]', isAwarded ? '' : 'grayscale')}/>
+        <p className='text-sm font-medium'>{badge.name}</p>
       </div>
-    </Card>
-  );
+    </DrawerTrigger>
+    ) : (
+      <div className='flex flex-col items-center gap-2'>
+        <img src={getBadgeImgUrl(badge.image_path)} alt={badge.name} className={twMerge('min-w-[150px]', isAwarded ? '' : 'grayscale')}/>
+        <p className='text-sm font-medium'>{badge.name}</p>
+      </div>
+    )}
+    <DrawerContent className='h-[50vh]'>
+      <DrawerHeader className='pt-8'>
+        <div className='flex flex-col gap-6 justify-center items-center text-center'>
+          <p className='font-medium'>{getType(badge.type)}</p>
+          <div className='flex flex-col gap-2'>
+            <DrawerTitle>{badge.title}</DrawerTitle>
+            <DrawerDescription>{badge.description}</DrawerDescription>
+          </div>
+        </div>
+      </DrawerHeader>
+      <div className='flex flex-col mx-auto items-center shadow-md w-1/2 p-5 rounded-2xl'>
+        <img src={getBadgeImgUrl(badge.image_path)} alt={badge.name} className='min-w-[200px]'/>
+        <h1 className='font-bold text-xl'>{badge.name}</h1>
+        <h1 className='font-medium text-sm mt-1'>{dateFormatter(awardedAt)}</h1>
+      </div>
+    </DrawerContent>
+  </Drawer>
+  )
 }
