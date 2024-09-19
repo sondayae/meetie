a6d4b3e81994f4b9b4d8b0446050b930934485cd
황준용 <wnsdyd3333@gmail.com>
2024-09-19 13:00:17 +0900

    사용자 인증 및 경로 제한에 따른 리다이렉트 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
 import { type NextRequest } from 'next/server';
 
-import { updateSession } from '@/utils/supabase/middleware';
+import { requireAuth } from '@/utils/middleware/requireAuth';
 
-// TODO 임시로 모두 막았지만 후에 config 경로 정리해야함
 export async function middleware(request: NextRequest) {
-  // return updateSession(request);
+  return requireAuth(request);
 }
 
 export const config = {
-  matcher: ['/'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
---
+++ src/utils/middleware/requireAuth.ts
@@ -0,0 +1,82 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { createServerClient } from '@supabase/ssr';
+
+export async function requireAuth(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({ request });
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({ request });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const publicPaths = ['/login', '/signup', '/', '/login/find/password'];
+  const isPublicPath = publicPaths.includes(request.nextUrl.pathname);
+  const isRestrictedPath =
+    request.nextUrl.pathname === '/login/update/password';
+
+  if (isRestrictedPath) {
+    if (user) {
+      // 로그인한 사용자가 `/login/update/password`경로로 접근 시 participating_study에 따라 리다이렉트
+      const { data } = await supabase
+        .from('user')
+        .select('participating_study')
+        .eq('id', user.id)
+        .single();
+
+      const url = request.nextUrl.clone();
+      url.pathname = data?.participating_study
+        ? `/studyroom/${data.participating_study}/calendar`
+        : '/studyroom';
+      return NextResponse.redirect(url);
+    }
+    // 로그인한 사용자가 `/login/update/password` 경로로 접근 시 루트로 리다이렉트
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
+  if (user) {
+    if (isPublicPath) {
+      // 로그인한 사용자가 publicPaths에 포함된 경로로 접근 시 participating_study에 따라 리다이렉트
+      const { data } = await supabase
+        .from('user')
+        .select('participating_study')
+        .eq('id', user.id)
+        .single();
+
+      const url = request.nextUrl.clone();
+      url.pathname = data?.participating_study
+        ? `/studyroom/${data.participating_study}/calendar`
+        : '/studyroom';
+      return NextResponse.redirect(url);
+    }
+  } else if (!isPublicPath) {
+    // 로그인한 사용자가 publicPaths 외의 경로로 접근 시 루트로 리다이렉트
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
+  return supabaseResponse;
+}
